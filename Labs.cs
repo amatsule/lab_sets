a3f194e1751a4c6c6483fb02829cea79f04cb179
janmats <96205973+janmats@users.noreply.github.com>
2022-09-12 20:26:58 +0300

    blin eto commit

--- Labs.cs
+++ Labs.cs
@@ -12,6 +12,7 @@ namespace Set
         {
             Console.OutputEncoding = Encoding.Unicode;
             // Создаем множества.
+            var universum = newuniversum(20);
             var setA = new Set<int>()
             {
                 1, 2, 3, 4, 5, 6
@@ -26,16 +27,22 @@ namespace Set
             {
                 2, 3, 4
             };
+            var notB = notset(setB, universum);
             var unionpart = Set<int>.Union(setA, setB);
             var union = Set<int>.Union(setC, unionpart);
+            var set1 = Set<int>.Intersection(setC, setA);
+            var set2 = Set<int>.Union(set1, notB);
 
             // Выводим исходные множества на консоль.
             PrintSet(setA, "Первое множество: ");
             PrintSet(setB, "Второе множество: ");
             PrintSet(setC, "Третье множество: ");
+            PrintSet(universum, "Универсум: ");
+            PrintSet(notB, "Не второе множество: ");
 
             // Выводим результирующее множество на консоль.
             PrintSet(union, "A U B U C: ");
+            PrintSet(set2, "C пересечение A Обьединение с не B ");
 
             Console.ReadLine();
         }
@@ -49,5 +56,30 @@ namespace Set
             }
             Console.WriteLine();
         }
+
+        private static Set<int> newuniversum(int n)
+        {
+            var resultSet = new Set<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                resultSet.Add(i);
+            }
+            return resultSet;
+        }
+
+        private static Set<int> notset(Set<int> set, Set<int> universum)
+        {
+            var resultSet = new Set<int>();
+            foreach (int item in universum)
+            {
+                if (!set.Contains(item))
+                {
+                    resultSet.Add(item);
+                } 
+            }
+
+            return resultSet;
+        }
+
     }
 }
